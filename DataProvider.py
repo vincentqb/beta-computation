7f4ae0f1e3096f007213eae3d09d19589ebcaf2b
Vincent <vincentqb@gmail.com>
2018-10-24 00:55:30 -0400

    use pandas data reader.

--- DataProvider.py
+++ DataProvider.py
@@ -1,7 +1,8 @@
+import os
 import urllib.request
 import configparser
 import pandas as pd
-import quandl
+from pandas_datareader import data
 from functools import reduce
 
 
@@ -13,25 +14,37 @@ API_KEY = config["QUANDL"]["API_KEY"]
 DB_NAME = config["QUANDL"]["DB_NAME"]
 VALUE_COL = config["QUANDL"]["VALUE_COL"]
 
-quandl.ApiConfig.api_key = API_KEY
+os.environ["QUANDL_API_KEY"] = API_KEY
 
 
-def get_security(code, start_date, end_date):
+def get_spy(start_date, end_date):
     """
-    Get given security for given date range.
+    Get SPY for given date range.
 
-    :param code: security to get
     :param start_date: start date
     :param end_date: end date
     :return: data frame with given security
     """
-    code_long = DB_NAME + "/" + code
-    return quandl.get(
-        code_long,
-        start_date=start_date,
-        end_date=end_date,
-        returns="pandas"
-    )[VALUE_COL].to_frame(name=code)
+
+    dummy_ind = "AAPL"
+    ind = "SPY"
+    return (
+            get_securities([dummy_ind, ind], start_date, end_date)
+            .drop(dummy_ind, axis=1)
+    )
+
+
+def get_securities(codes, start_date, end_date):
+    """
+    Get given security for given date range.
+
+    :param codes: list of securities to get
+    :param start_date: start date
+    :param end_date: end date
+    :return: data frame with given security
+    """
+
+    return data.DataReader(codes, 'quandl', start_date, end_date)[VALUE_COL]
 
 
 def get_codes(n):
@@ -58,7 +71,7 @@ def get_codes(n):
     return list(df_codes["code"].sample(n))
 
 
-def get_securities(n, start_date, end_date):
+def get_random_securities(n, start_date, end_date):
     """
     Get n random securities for given date range.
 
@@ -74,13 +87,4 @@ def get_securities(n, start_date, end_date):
 
     # Get securities
 
-    dfs = []
-
-    for code in codes:
-        df = get_security(code, start_date, end_date)
-        dfs.append(df)
-
-    return reduce(
-            lambda left, right: pd.merge(left, right, on="Date"),
-            dfs
-    )
+    return get_security(codes, start_date, end_date)
